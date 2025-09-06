f54af65e129b5f08bf4d0f86d87275f7566228b3
smassely <smassollol@gmail.com>
2025-09-06 20:47:26 +0100

    added item registries

--- src/main/java/com/smassely/modone/item/ModItems.java
+++ src/main/java/com/smassely/modone/item/ModItems.java
@@ -1,15 +1,21 @@
 package com.smassely.modone.item;
 
 import com.smassely.modone.ModOne;
+import com.smassely.modone.item.custom.DeadBoneSwordItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+
+    public static final Item DEADBONESWORD = registerItem("dead_bone_sword", new DeadBoneSwordItem(ToolMaterials.DIAMOND,3,3.0f, new FabricItemSettings()));
 
     public static void addItemsToIngredientGroup(FabricItemGroupEntries entries){
 
--- src/main/java/com/smassely/modone/item/custom/DeadBoneSwordItem.java
+++ src/main/java/com/smassely/modone/item/custom/DeadBoneSwordItem.java
@@ -1,4 +1,42 @@
 package com.smassely.modone.item.custom;
 
-public class DeadBoneSwordItem {
+import com.smassely.modone.ModOne;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.entity.projectile.WitherSkullEntity;
+import net.minecraft.entity.projectile.thrown.ThrownEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+import static com.ibm.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
+public class DeadBoneSwordItem extends SwordItem {
+    public DeadBoneSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+
+        if (!world.isClient){
+            WitherSkullEntity skul = new WitherSkullEntity(EntityType.WITHER_SKULL, world);
+            skul.setPosition(user.getX(), user.getEyeY(), user.getZ());
+            skul.setOwner(user);
+            skul.setVelocity(user, user.getPitch(), user.getYaw(),0f,3f,0f);
+
+            world.spawnEntity(skul);
+            ModOne.LOGGER.info("clicked");
+            if (!user.isCreative()){
+
+
+            }
+        }
+
+        return super.use(world, user, hand);
+    }
 }
