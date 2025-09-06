058fa66137203815c3401983af3457f05694fc61
smassely <smassollol@gmail.com>
2025-09-06 17:30:23 +0100

    Start of Modone

---
+++ src/client/java/com/smassely/modone/ModOneDataGenerator.java
@@ -0,0 +1,11 @@
+package com.smassely.modone;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModOneDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
