9d2795376e882fd84a8a6f38562f3a76181b78d1
smassely <smassollol@gmail.com>
2025-09-07 20:29:14 +0100

    created texture. not added yet

--- src/main/java/com/smassely/modone/item/custom/DeadBoneSwordItem.java
+++ src/main/java/com/smassely/modone/item/custom/DeadBoneSwordItem.java
@@ -1,11 +1,8 @@
 package com.smassely.modone.item.custom;
 
-import com.smassely.modone.ModOne;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.entity.projectile.WitherSkullEntity;
-import net.minecraft.entity.projectile.thrown.ThrownEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
@@ -34,7 +31,6 @@ public class DeadBoneSwordItem extends SwordItem {
             skull.setVelocity(user, user.getPitch(), user.getYaw(), 0f, 3f, 0f);
             world.spawnEntity(skull);
 
-            ModOne.LOGGER.info("clicked");
 
             if (!user.isCreative()) {
                 if (user.getStackInHand(hand).getDamage() <= user.getStackInHand(hand).getMaxDamage() - 1) {
@@ -45,7 +41,6 @@ public class DeadBoneSwordItem extends SwordItem {
             user.getItemCooldownManager().set(this, 15);
         }
 
-//        return TypedActionResult.success(itemStack);
         return super.use(world,user,hand);
     }
 }
